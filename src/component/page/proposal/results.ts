3065c4d02973ad81ecf4b1e05ba34458421a453d
aggre <hiroyuki.aggre@gmail.com>
2021-03-31 14:19:40 +0900

    add progressbar

--- src/component/page/proposal/results.ts
+++ src/component/page/proposal/results.ts
@@ -7,12 +7,12 @@ import { Attributes } from '../../../lib/vote/attributes'
 import { getVotes } from '../../../mock/@devprotocol/governance'
 import { ul } from '../../../style/reset/ul'
 import { BigNumber, constants } from 'ethers'
-import { whenDefined } from '@devprotocol/util-ts'
 import { asVar } from '../../../style/custom-properties'
 import { asideHeading, asideContainer } from './styles'
 
-const share = (num: BigNumber, total: BigNumber): number =>
-	num.div(total).mul(100).toNumber()
+const BASIS = 10000
+const calcShare = (num: BigNumber, total: BigNumber): number =>
+	num.mul(BASIS).div(total).mul(100).toNumber() / BASIS
 
 export const results = (
 	contractAddress: string,
@@ -28,6 +28,17 @@ export const results = (
 				color: ${asVar('weakColor')};
 				font-family: monospace;
 			}
+			[role='progressbar'] {
+				height: 0.3rem;
+				overflow: hidden;
+				background: ${asVar('weakColor')};
+				border-radius: ${asVar('borderRadius')};
+			}
+			[role='progressbar'] > span {
+				height: 100%;
+				display: block;
+				background: ${asVar('primaryVariantColor')};
+			}
 		</style>
 		${subscribe(from(getVotes(contractAddress)), (data) => {
 			const total = data.reduce((p, x) => p.add(x.count), constants.Zero)
@@ -38,17 +49,25 @@ export const results = (
 						${repeat(options, (option, i) => {
 							const result = data[i]
 							const { count, counts } = result
-							return whenDefined(result, (x) =>
-								(([heading]) => html`
-									<li>
-										<span>${heading} (${share(count, total)}%)</span>
-										<span>#1 ${counts[0].toNumber()} votes</span>
-										<span>#2 ${counts[1].toNumber()} votes</span>
-										<span>#3 ${counts[2].toNumber()} votes</span>
-										<span>Borda Count ${count.toNumber()}</span>
-									</li>
-								`)(findHeadings(option))
-							)
+							const share = calcShare(count, total)
+							return (([heading]) => html`
+								<li>
+									<span>${heading} (${share}%)</span>
+									<div
+										role="progressbar"
+										aria-valuenow=${share}
+										aria-valuemin="0"
+										aria-valuemax="100"
+										max="100"
+									>
+										<span style="width: ${share}%" role="presentation"></span>
+									</div>
+									<span>#1 ${counts[0].toNumber()} votes</span>
+									<span>#2 ${counts[1].toNumber()} votes</span>
+									<span>#3 ${counts[2].toNumber()} votes</span>
+									<span>Borda Count ${count.toNumber()}</span>
+								</li>
+							`)(findHeadings(option))
 						})}
 					</ul>
 				</section>
