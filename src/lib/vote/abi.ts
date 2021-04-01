49a916137b47d04c6ddb8e70ef819814e503cd30
aggre <hiroyuki.aggre@gmail.com>
2021-04-01 12:49:18 +0900

    fix abi

--- src/lib/vote/abi.ts
+++ src/lib/vote/abi.ts
@@ -1,4 +1,4 @@
 export const voteAbi = [
 	'function attributes() external view returns (tuple(address proposer, string subject, string body, uint256 period, string[] options, string bodyMimeType, string optionsMimeType))',
-	'function vote(uint8[] memory _options, uint8[] memory percentiles) external',
+	'function vote(uint8[] memory percentiles) external',
 ]
