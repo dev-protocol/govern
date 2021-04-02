4d285332ce8cf997f96890b027f2f3738368e312
aggre <hiroyuki.aggre@gmail.com>
2021-04-02 18:54:29 +0900

    change endpoint from infura to alchemy

--- src/constant/contracts.ts
+++ src/constant/contracts.ts
@@ -1,4 +1,4 @@
 export const contracts = ['0x6c38f1556AeAC49ACFEAD034a08F4c2E7F255956']
 
 export const provider =
-	'https://mainnet.infura.io/v3/75ebe953349644b6998136d868f5cd97'
+	'https://eth-mainnet.alchemyapi.io/v2/n2p0F80jDrsvd-acSV53QoLSobPEWaNz'
