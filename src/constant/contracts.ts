e4436db88df2765772a4e0b4e6b48bbd485c4932
aggre <hiroyuki.aggre@gmail.com>
2021-04-23 15:47:07 +0900

    add the new proposal

--- src/constant/contracts.ts
+++ src/constant/contracts.ts
@@ -1,4 +1,7 @@
-export const contracts = ['0x6c38f1556AeAC49ACFEAD034a08F4c2E7F255956']
+export const contracts = [
+	'0xB78c2F61501C858D6eeF78AFCe3f11a0a31DcEcC',
+	'0x6c38f1556AeAC49ACFEAD034a08F4c2E7F255956',
+]
 
 export const provider =
 	'https://eth-mainnet.alchemyapi.io/v2/n2p0F80jDrsvd-acSV53QoLSobPEWaNz'
